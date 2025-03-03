902638d1582fda7fc09efca61dbb88b918810f6e
Arthur <Arthur@DESKTOP-ARTHUR>
2025-03-03 23:21:29 +0800

    Add project files.

---
+++ ENGG 1340 Group Project.cpp
@@ -0,0 +1,12 @@
+﻿// ENGG 1340 Group Project.cpp : Defines the entry point for the application.
+//
+
+#include "ENGG 1340 Group Project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
