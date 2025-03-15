028df4335452a049ab51e95b8771400846ab1436
HamburgerBoi <96848119arthur@gmail.com>
2025-03-15 16:57:15 +0800

    Created files & folders for modular file management Each file should contain one class only

--- ENGG 1340 Group Project.h
+++
@@ -1,8 +0,0 @@
-﻿// ENGG 1340 Group Project.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
