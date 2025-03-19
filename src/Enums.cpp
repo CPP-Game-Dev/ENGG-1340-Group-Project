5449d808af1a213fb8b460cd29201e6faea502b9
HamburgerBoi <68740916+Hamburger666@users.noreply.github.com>
2025-03-19 15:43:53 +0800

    fix: changed EntityTypes to TileObject in Enums

--- src/Enums.cpp
+++ src/Enums.cpp
@@ -3,11 +3,12 @@ File to store all enums used in other classes
 */
 
 /*
-enum for different pickups
+enum for different tiles 
 */
-enum EntityType
+enum TileObject
 {
-	RegenSmall, RegenMedium, RegenBig,			//3 tiers of instant Stamina replenish
+	Player, Wall, None,				//Player, Wall of maze, Path
+	RegenSmall, RegenMedium, RegenBig,		//3 tiers of instant Stamina replenish
 	StaminaSmall, StaminaMedium, StaminaBig,	//3 tiers of maximum Stamina boost
 	VisionSmall, VisionMedium, VisionBig,		//3 tiers of vision range boost
 };
