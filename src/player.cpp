5d398b6720e8a9c6b8a08ab2fd8914c731fa3e65
jljl-129 <leesoyeon2143@gmail.com>
2025-03-28 00:53:33 +0800

    updated player

--- src/player.cpp
+++ src/player.cpp
@@ -6,18 +6,16 @@
 Player::Player() {}
 
 Player::Player(Vector2D pos, int stamina, int staminaMax, int fov)
-    : position(pos), stamina(stamina), staminaMax(staminaMax), fieldofvision(fov) {
-    // TODO:Implementation
-}
+    : position(pos), stamina(stamina), staminaMax(staminaMax), fieldofvision(fov) {}
 
-//Getters implementation
-Vector2D player::getPosition() const {return position;}
-int Player::getStamina() const {return stamina;}
-int Player::getStaminaMax() const {return staminaMax;}
-int Player::getfieldofvision() const{return fieldofvision)
-    
-//Setters implementation
-void Player::setPosition(const Vector2D& pos) {position = pos;}
-void Player::setStamina(int s) {stamina= s;}
-void Player::setStaminaMax(int sMax) {staminaMax = sMax;}
-void Player::setfieldofvision(int fov) {fieldofvision = fov;}
+// Getters
+Vector2D Player::getPosition() const { return position; }
+int Player::getStamina() const { return stamina; }
+int Player::getStaminaMax() const { return staminaMax; }
+int Player::getfieldofvision() const { return fieldofvision; }
+
+// Setters
+void Player::setPosition(const Vector2D& pos) { position = pos; }
+void Player::setStamina(int s) { stamina = s; }
+void Player::setStaminaMax(int sMax) { staminaMax = sMax; }
+void Player::setfieldofvision(int fov) { fieldofvision = fov; }
