8db2b96b91475a1f84836e4f5c2ce31996c6ca25
jljl-129 <leesoyeon2143@gmail.com>
2025-03-29 03:41:16 +0800

    feat (player)
    implemented default and parametric constructors with getters and setters. I would review it again and commit changes when i have time tmr

--- src/player.cpp
+++ src/player.cpp
@@ -15,13 +15,31 @@ Player::Player() {
     * Pos is set to 0, 0
     * Ignore inventory, let me(Arthur) deal with it
     */ 
+    baseStamina = 0;
+    baseStaminaMax = 0;
+    baseRationRegen = 0;
+    baseFov = 0;
+    baseRationCapacity = 0;
+    basePickaxeCapacity = 0;
+
+    staminaMultiplier = 1;
+    rationRegenMultiplier = 1;
+    fovMultiplier = 1;
+    rationCapacityMultiplier = 1;
+    pickaxeCapacityMultiplier = 1;
+
+    position = Vector2D(0, 0);
 
     
 }
 
 Player::Player( int baseStamina, int baseStaminaMax, int baseRationRegen,
                 int baseFov, int baseRationCapacity, int basePickaxeCapacity,
-                Vector2D pos, std::vector<std::unique_ptr<IItem>> inventory) {
+                Vector2D pos, std::vector<std::unique_ptr<IItem>> inventory)
+    : baseStamina(baseStamina), baseStaminaMax(baseStaminaMax),
+      baseRationRegen(baseRationRegen), baseFov(baseFov),
+      baseRationCapacity(baseRationCapacity), basePickaxeCapacity(basePickaxeCapacity),
+      position(pos), inventory(std::move(inventory)) {
     /*
     * TODO(Jenna): Implement parametric constructor 
     * Set each class variable to the input value
@@ -32,7 +50,36 @@ Player::Player( int baseStamina, int baseStaminaMax, int baseRationRegen,
 
 #pragma region Getters
 //TODO(Jenna): Implement getters according to their declarations in player.h 
+int Player::getBaseStamina() const { return baseStamina; }
+int Player::getBaseStaminaMax() const { return baseStaminaMax; }
+int Player::getBaseRationRegen() const { return baseRationRegen; }
+int Player::getBaseFov() const { return baseFov; }
+int Player::getBaseRationCapacity() const { return baseRationCapacity; }
+int Player::getBasePickaxeCapacity() const { return basePickaxeCapacity; }
+
+float Player::getStaminaMultiplier() const { return staminaMultiplier; }
+float Player::getRationRegenMultiplier() const { return rationRegenMultiplier; }
+float Player::getFovMultiplier() const { return fovMultiplier; }
+float Player::getRationCapacityMultiplier() const { return rationCapacityMultiplier; }
+float Player::getPickaxeCapacityMultiplier() const { return pickaxeCapacityMultiplier; }
+
 #pragma endregion
 #pragma region Setters
 //TODO(Jenna): Implement setters according to their declarations in player.h
+
+void Player::setBaseStamina(int value) { baseStamina = value; }
+void Player::setBaseStaminaMax(int value) { baseStaminaMax = value; }
+void Player::setBaseRationRegen(int value) { baseRationRegen = value; }
+void Player::setBaseFov(int value) { baseFov = value; }
+void Player::setBaseRationCapacity(int value) { baseRationCapacity = value; }
+void Player::setBasePickaxeCapacity(int value) { basePickaxeCapacity = value; }
+
+void Player::setStaminaMultiplier(float value) { staminaMultiplier = value; }
+void Player::setRationRegenMultiplier(float value) { rationRegenMultiplier = value; }
+void Player::setFovMultiplier(float value) { fovMultiplier = value; }
+void Player::setRationCapacityMultiplier(float value) { rationCapacityMultiplier = value; }
+void Player::setPickaxeCapacityMultiplier(float value) { pickaxeCapacityMultiplier = value; }
+
+void Player::setPosition(const Vector2D& pos) { position = pos; }
+
 #pragma endregion
