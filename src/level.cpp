7f62c3216874c39b390c8d424e071166528cb1a2
HamburgerBoi <68740916+Hamburger666@users.noreply.github.com>
2025-04-02 05:49:22 +0000

    fix: fixed hard-coded parts in item generation & removed debug print function(it's obsolete)

--- src/level.cpp
+++ src/level.cpp
@@ -127,20 +127,20 @@ void Level::placeItems(int itemCount) {
 
 #pragma region Weighted Randomization
     // Hard-coded weights & respective items
-    int weights[9] = {20, 10, 5, 15, 7, 3, 5, 2, 1};
-    TileObject items[9] = {RationSmall,  RationMedium,  RationBig,
-                           StaminaSmall, StaminaMedium, StaminaBig,
-                           VisionSmall,  VisionMedium,  VisionBig};
+    int weights[6] = { 45, 20, 5, 10, 15, 5};
+    TileObject items[6] = {TileObject::Ration,  TileObject::EnergyDrink,
+                           TileObject::Battery, TileObject::Pickaxe,
+                           TileObject::Chest,  TileObject::Mimic};
 
     // Calculate sum of weights
     int weightSum = 0;
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 6; i++)
         weightSum += weights[i];
 
     // Place a given amount of items
     for (int i = 0; i < itemCount; i++) {
         int rnd = int(std::rand() % weightSum);
-        for (int j = 0; j < itemCount; j++) {
+        for (int j = 0; j < 6; j++) {
             if (rnd < weights[j]) {
                 // Get random empty tile
                 Vector2D tilePos =
@@ -164,65 +164,6 @@ void Level::setEndpoint() {
         }
     }
 }
-
-/*
- * == FOR DEBUG PURPOSES ONLY ==
- *
- * Pretty Print function, prints according to tile types
- * Does not require ncurses
- */
-void Level::print() {
-    system("clear");
-    // Lambda function to convert TileObject into Char
-    auto tts = [](TileObject tile) {
-        switch (tile) {
-        case Player:
-            return "P";
-        case Wall:
-            return "#";
-        case RationSmall:
-            return "1";
-        case RationMedium:
-            return "2";
-        case RationBig:
-            return "3";
-        case StaminaSmall:
-            return "4";
-        case StaminaMedium:
-            return "5";
-        case StaminaBig:
-            return "6";
-        case VisionSmall:
-            return "7";
-        case VisionMedium:
-            return "8";
-        case VisionBig:
-            return "9";
-        default:
-            return " ";
-        }
-    };
-
-    std::vector<std::string> rows;
-    std::string horizontalBar = "#";
-    for (int i = 0; i < this->size + 1; i++)
-        horizontalBar.append("#");
-    rows.push_back(horizontalBar);
-    std::string row;
-    for (int i = 0; i < this->size; i++) {
-        row = "#";
-        for (int j = 0; j < this->size; j++) {
-            row.append(tts(this->maze[i][j]));
-        }
-        row.append("#");
-        rows.push_back(row);
-    }
-    rows.push_back(horizontalBar);
-    for (int i = 0; i < this->size + 2; i++) {
-        std::cout << rows[i] << std::endl;
-    }
-}
-
 int Level::getSize() const { return this->size; }
 
 std::vector<std::vector<TileObject>> Level::getMaze() const { return this->maze; }
