7a6eaee1fee1b1cb0a1d244de6443e98918db76f
invalidcode232 <mail@invalidcode.me>
2025-04-02 16:04:48 +0800

    fix(display): include vector2d

--- src/display.cpp
+++ src/display.cpp
@@ -1,17 +1,17 @@
 #include "include/display.h"
 #include "include/level.h"
-#include "vector2d.h"
+#include "include/vector2d.h"
 #include <ncurses.h>
 
 #define MEGAPIXEL "##"
 
-Display::Display() {
-    // TODO:Implementation
-}
+// Display::Display() {
+//     // TODO:Implementation
+// }
 
 /*
-* Function to initialize nCurses
-*/
+ * Function to initialize nCurses
+ */
 void Display::initCurses() {
     initscr();
     start_color(); // Enable color functionality
@@ -25,45 +25,47 @@ void Display::initCurses() {
     init_pair(3, COLOR_BLACK, COLOR_BLACK);  // None
     init_pair(4, COLOR_GREEN, COLOR_BLACK);  // Extras
     init_pair(5, COLOR_BLACK, COLOR_CYAN);   // Hidden
-
 }
 
 /*
-* Function to flush(clear) the screen
-*/
+ * Function to flush(clear) the screen
+ */
 void Display::flush() {
     clear();
     refresh();
 }
 
 void Display::drawLevel(Level level) {
-    //TODO(Chris): Implement Manhatton distance based radius calculation
-    //             & related drawing calculations
+    // TODO(Chris): Implement Manhatton distance based radius calculation
+    //              & related drawing calculations
 
     // Initialize an anchor point (topleft of the maze)
     Vector2D maxSize;
     getmaxyx(stdscr, maxSize.y, maxSize.x);
-    Vector2D anchor = Vector2D( int((maxSize.y - level.getSize())/2), 
-                                int((maxSize.x - level.getSize())/2));
-    std::vector<std::vector<TileObject>> maze = level.getMaze();
+    Vector2D anchor = Vector2D(int((maxSize.y - level.getSize()) / 2),
+                               int((maxSize.x - level.getSize()) / 2));
+    std::vector<std::vector<TileObject> > maze = level.getMaze();
 
-    auto getColors = [](TileObject tile){
-        switch(tile){
-            case TileObject::Player: return 1;
-            case TileObject::Wall: return 2;
-            case TileObject::None: return 3;
-            default: return 4;
+    auto getColors = [](TileObject tile) {
+        switch (tile) {
+        case TileObject::Player:
+            return 1;
+        case TileObject::Wall:
+            return 2;
+        case TileObject::None:
+            return 3;
+        default:
+            return 4;
         }
     };
 
-    for(int i = 0; i < level.getSize(); i++) {
+    for (int i = 0; i < level.getSize(); i++) {
         move(anchor.y + i, anchor.x);
-        for(int j = 0; j < level.getSize(); j++) {
+        for (int j = 0; j < level.getSize(); j++) {
             int colors = getColors(maze[i][j]);
             attron(COLOR_PAIR(colors));
             addstr(MEGAPIXEL);
             attroff(COLOR_PAIR(colors));
         }
     }
-    
 }
--- src/include/player.h
+++ src/include/player.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "vector2d.h"
 #include "iitem.h"
+#include "vector2d.h"
 #include <memory>
 #include <vector>
 
@@ -10,46 +10,51 @@
  */
 class Player {
   private:
-    #pragma region Base Stats
-    int baseStaminaMax;       // The max amount of stamina that can be carried at any time
-    int baseRationRegen;      // The amount of stamina regenerated when using a ration
-    int baseFov;              // The radius of FOV
-    int baseRationCapacity;   // The number of rations that can be carried
-    int basePickaxeCapacity;  // The number of pickaxes that can be carried
-    #pragma endregion
+#pragma region Base Stats
+    int baseStaminaMax;  // The max amount of stamina that can be carried at any
+                         // time
+    int baseRationRegen; // The amount of stamina regenerated when using a
+                         // ration
+    int baseFov;         // The radius of FOV
+    int baseRationCapacity;  // The number of rations that can be carried
+    int basePickaxeCapacity; // The number of pickaxes that can be carried
+#pragma endregion
 
-    std::vector<std::unique_ptr<IItem>> inventory;  // Inventory storage
+    std::vector<std::unique_ptr<IItem> > inventory; // Inventory storage
 
   public:
-    #pragma region Current Stats
-    int stamina;          // Current stamina  
-    int staminaMax;       // Current max stamina
-    int rationRegen;      // Current ration regeneration amount
-    int fov;              // Current FOV
-    int rationCapacity;   // Current ration capacity
-    int pickaxeCapacity;  // Current pickaxe capacity
-    Vector2D pos;         // Current position
-    #pragma region endregion
-    #pragma region Stat Multipliers
+#pragma region Current Stats
+    int stamina;         // Current stamina
+    int staminaMax;      // Current max stamina
+    int rationRegen;     // Current ration regeneration amount
+    int fov;             // Current FOV
+    int rationCapacity;  // Current ration capacity
+    int pickaxeCapacity; // Current pickaxe capacity
+    Vector2D pos;        // Current position
+#pragma region endregion
+#pragma region Stat Multipliers
     double staminaMaxMult;      // Multiplier for max stamina
     double rationRegenMult;     // Multiplier for ration stamina regen
     double fovMult;             // Multiplier for FOV
     double rationCapacityMult;  // Multiplier for ration capacity
     double pickaxeCapacityMult; // Multiplier for pickaxe capacity
-    #pragma endregion
+#pragma endregion
 
     Player(); // Initialize a new Player object (used for new game)
-    Player( int baseStamina, int baseStaminaMax, int baseRationRegen,
-            int baseFov, int baseRationCapacity, int basePickaxeCapacity,
-            Vector2D pos, std::vector<std::unique_ptr<IItem>> inventory); // Set player base stats, pos & inventory to given values
+    Player(int baseStamina, int baseStaminaMax, int baseRationRegen,
+           int baseFov, int baseRationCapacity, int basePickaxeCapacity,
+           Vector2D pos,
+           std::vector<std::unique_ptr<IItem> >
+               inventory); // Set player base stats, pos & inventory to given
+                           // values
 
     void addItem(std::unique_ptr<IItem>);
     void removeItem(std::unique_ptr<IItem>);
 
-    #pragma region Getters
-    //TOD(Jenna):Declare getters for all base stats
-    #pragma endregion
-    #pragma region Setters
-    //TODO(Jenna):Declare setters for all base stats
-    #pragma endregion
+#pragma region Getters
+// TOD(Jenna):Declare getters for all base stats
+#pragma endregion
+#pragma region Setters
+// TODO(Jenna):Declare setters for all base stats
+#pragma endregion
 };
