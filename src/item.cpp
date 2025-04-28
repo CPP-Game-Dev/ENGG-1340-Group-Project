a8420e914f863312a1e48799cd02c753d9ebab1a
jljl-129 <leesoyeon2143@gmail.com>
2025-04-28 23:46:28 +0800

    Add overloaded item constructor to support CSV-based initialisation
    - added a new constructor for Item that takes id, name, description and property

--- src/item.cpp
+++ src/item.cpp
@@ -27,4 +27,22 @@ Item::Item(ItemID id) {
     this->bonusRationCapacityMult = 0.0;
     this->bonusPickaxeCapacityMult = 0.0;
 }
+Item::Item(ItemID id, const std::string& name, const std::string& description, int property) {
+this->id = id;
+this->name = name;
+this->description = description;
+this->rarity = property;
 
+this->counter = 0;
+
+this->bonusStaminaMax = 0;
+this->bonusRationRegen = 0;
+this->bonusFov = 0;
+this->bonusRationCapacity = 0;
+this->bonusPickaxeCapacity = 0;
+    
+this->bonusStaminaMaxMult = 0.0;
+this->bonusRationRegenMult = 0.0;
+this->bonusFovMult = 0.0;
+this->bonusRationCapacityMult = 0.0;
+this->bonusPickaxeCapacityMult = 0.0;
