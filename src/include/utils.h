ac7d1711ac960aaac8fdac7b531887f7d9a5ca73
jljl-129 <leesoyeon2143@gmail.com>
2025-04-28 23:58:37 +0800

    add createitem functions to utils for item creation from CSV

--- src/include/utils.h
+++ src/include/utils.h
@@ -2,6 +2,9 @@
 
 #include <memory>
 #include <assert.h>
+#include <string>
+#include "item.h"
+#include "enums.h"
 
 namespace utils {
 
@@ -10,5 +13,11 @@ namespace utils {
     {
         static_assert(!std::is_array<T>::value, "arrays not supported");
         return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+    inline std::unique_ptr<Item> createItem(ItemID id) {
+        return utils::make_unique<Item>(id);
+    }
+    inline std::unique_ptr<Item> createItem(ItemID id, const std::string& name, const std::string& descrription, int property) {
+        return utils::make_unique<Item>(id, name, description, property);
     }
 }
