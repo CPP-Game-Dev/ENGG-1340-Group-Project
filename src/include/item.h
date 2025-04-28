790f0a7fd4344b302c4855e85dd3ec823569ace7
jljl-129 <leesoyeon2143@gmail.com>
2025-04-29 01:43:04 +0800

    refactor item constructor declaration

--- src/include/item.h
+++ src/include/item.h
@@ -31,10 +31,10 @@ class Item {
   //setting all the bonuses and counters to 0.
     Item(ItemID id);
 //overloaded constructor:
-    Item(ItemID id, const std::string& name,
-    const std::string& description, int property);
+    Item(ItemID id, const std::string& name, const std::string& description, int property);
 //initialises an item with specific values.
 //used in loading items data 
+//implementation
   
 };
 
