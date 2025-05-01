4029651560306cf54bbabbdd1fc5f895cd30827e
invalidcode232 <mail@invalidcode.me>
2025-05-01 12:44:53 +0800

    test(HUD): change enum name

--- test/config.cpp
+++ test/config.cpp
@@ -105,7 +105,7 @@ void test_default_config_creation() {
     assert(config.getConfig(Config::KB_RIGHT) == "d");
     assert(config.getConfig(Config::KB_USE_PICKAXE) == "e");
     assert(config.getConfig(Config::KB_USE_RATION) == "r");
-    assert(config.getConfig(Config::KB_QUIT) == "q");
+    assert(config.getConfig(Config::KB_EXIT) == "q");
 
     std::cout << "Default config creation test passed!" << std::endl;
 }
